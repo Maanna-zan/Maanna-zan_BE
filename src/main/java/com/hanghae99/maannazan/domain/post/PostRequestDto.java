09ad6d10662ec985c1076ee2e532ad3ab16c33d7
Yonghyun Lee <dldydgus95426@gmail.com>
2023-04-01 21:00:59 +0900

    feat : 서비스 작성할 것!

--- src/main/java/com/hanghae99/maannazan/domain/post/PostRequestDto.java
+++ src/main/java/com/hanghae99/maannazan/domain/post/PostRequestDto.java
@@ -6,4 +6,14 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class PostRequestDto {
+
+    private String title;
+    private String description;
+    private String image;
+
+    public PostRequestDto(PostRequestDto postRequestDto) {
+        this.title = postRequestDto.getTitle();
+        this.description = postRequestDto.getDescription();
+        this.image = postRequestDto.getImage();
+    }
 }
--- src/main/java/com/hanghae99/maannazan/domain/post/PostResponseDto.java
+++ src/main/java/com/hanghae99/maannazan/domain/post/PostResponseDto.java
@@ -6,5 +6,15 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class PostResponseDto {
+
+    private Long id;
+    private String title;
+    private String description;
+    private String image;
+    private String nickname;
+    private String createdate;
+    private String modifidate;
+    private boolean likeCheck;
+    private int likeCnt;
+
 }
-//
