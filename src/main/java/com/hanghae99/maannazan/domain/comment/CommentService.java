57ad598fe5b962743052a29ae9653c1566135ef2
shanhe95 <sanha0630@naver.com>
2023-04-24 17:57:04 +0900

    refactor : CommentService 단일책임원칙 준수 리팩토링 완료

--- src/main/java/com/hanghae99/maannazan/domain/comment/CommentService.java
+++ src/main/java/com/hanghae99/maannazan/domain/comment/CommentService.java
@@ -2,7 +2,6 @@ package com.hanghae99.maannazan.domain.comment;
 
 
 import com.hanghae99.maannazan.domain.comment.dto.CommentRequestDto;
-import com.hanghae99.maannazan.domain.comment.dto.CommentResponseDto;
 import com.hanghae99.maannazan.domain.entity.Comment;
 import com.hanghae99.maannazan.domain.entity.Post;
 import com.hanghae99.maannazan.domain.entity.User;
@@ -10,10 +9,7 @@ import com.hanghae99.maannazan.domain.repository.CommentRepository;
 import com.hanghae99.maannazan.domain.repository.PostRepository;
 import com.hanghae99.maannazan.global.exception.CustomErrorCode;
 import com.hanghae99.maannazan.global.exception.CustomException;
-import com.hanghae99.maannazan.global.exception.ResponseMessage;
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -42,9 +38,7 @@ public class CommentService {
     public void updateComment(Long commentId, CommentRequestDto commentRequestDto, User user) {
 
 //        댓글 존재 여부 확인. 없으면 예외처리
-        Comment comment = commentRepository.findById(commentId).orElseThrow(
-                () -> new CustomException(CustomErrorCode.COMMENT_NOT_FOUND)
-        );
+        Comment comment = getCommentOne(commentId);
 //        ADMIN이 아닌 멤버가 댓글의 해당 작성자가 아닐때 예외 처리 && !user.getRole().equals(UserRoleEnum.ADMIN
         if (!comment.getUser().getId().equals(user.getId())) {
             throw new CustomException(CustomErrorCode.NOT_AUTHOR);
@@ -55,31 +49,27 @@ public class CommentService {
 
     //    3. 댓글 삭제 메서드
     @Transactional
-    public ResponseEntity deleteComment(Long commentId, User user) {
+    public void deleteComment(Long commentId, User user) {
         //        게시글 존재 여부 확인. 없으면 예외처리
 
 //        댓글 존재 여부 확인. 없으면 예외처리
-        Comment comment = commentRepository.findById(commentId).orElseThrow(
-                () -> new CustomException(CustomErrorCode.COMMENT_NOT_FOUND)
-        );
+        Comment comment = getCommentOne(commentId);
 //        ADMIN이 아닌 멤버가 댓글의 해당 작성자가 아닐때 예외 처리 && !user.getRole().equals(UserRoleEnum.ADMIN)
         if (!comment.getUser().getId().equals(user.getId())) {
             throw new CustomException(CustomErrorCode.NOT_AUTHOR);
         }
         commentRepository.deleteById(commentId);
-        return ResponseMessage.SuccessResponse("삭제 성공", "");
     }
 
     //대댓글 작성
     @Transactional
-    public CommentResponseDto createCommentList(CommentRequestDto commentRequestDto, User user, Long parentId) {
+    public void createCommentList(CommentRequestDto commentRequestDto, User user, Long parentId) {
 
         Comment parentComment = null;
         if (parentId != null) {
             parentComment = commentRepository.findById(parentId)
                     .orElseThrow(() -> new CustomException(CustomErrorCode.COMMENT_NOT_FOUND));
         }
-        System.out.println(user+ user.getUserName());
         Comment comment = new Comment(commentRequestDto, user, parentComment);
         commentRepository.saveAndFlush(comment);
 
@@ -88,13 +78,12 @@ public class CommentService {
             commentRepository.saveAndFlush(parentComment);
         }
 
-        return new CommentResponseDto(comment);
+
     }
 
     @Transactional
     public void deleteCommentList(Long commentId, User user) {
-        Comment comment = commentRepository.findById(commentId)
-                .orElseThrow(() -> new CustomException(CustomErrorCode.COMMENT_NOT_FOUND));
+        Comment comment = getCommentOne(commentId);
         Long parentId = null;
         if (comment.getParent() != null) {
             parentId = comment.getParent().getId();
@@ -117,9 +106,7 @@ public class CommentService {
 
     @Transactional
     public void updateCommentList(Long commentId, User user, CommentRequestDto commentRequestDto) {
-        Comment comment = commentRepository.findById(commentId)
-                .orElseThrow(() -> new CustomException(CustomErrorCode.COMMENT_NOT_FOUND));
-
+        Comment comment = getCommentOne(commentId);
         // 해당 댓글을 수정할 권한이 있는지 체크.
         if (!comment.getUser().getId().equals(user.getId())) {
             throw new CustomException(CustomErrorCode.NOT_AUTHOR);
@@ -135,5 +122,19 @@ public class CommentService {
                 updateComment(childComment.getId(), commentRequestDto, user);
             }
         }
+    }
+
+
+
+    //메서드
+    public List<Comment> getCommentListByPost(Post post){    // 게시글에 달린 댓글리스트 조회
+        return commentRepository.findByPost(post);
+    }
+
+
+    public Comment getCommentOne(Long commentId) {    // 단일 댓글 찾기(업데이트, 삭제)
+        return commentRepository.findById(commentId).orElseThrow(
+                () -> new CustomException(CustomErrorCode.COMMENT_NOT_FOUND)
+        );
     }
 }
