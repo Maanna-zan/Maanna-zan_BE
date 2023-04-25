5117927835de4c8e972b69dd2e5de894d7b98c53
shanhe95 <sanha0630@naver.com>
2023-04-25 18:30:31 +0900

    feat : 술집 다중 검색 기능 구현완료 (하지만 다중 검색 시 param 값을 4개를 받아와야 하는 상황 발생, 방법을 찾아 추후 코드 수정 필요해보임)

--- src/main/java/com/hanghae99/maannazan/domain/kakaoapi/KakaoApi.java
+++ src/main/java/com/hanghae99/maannazan/domain/kakaoapi/KakaoApi.java
@@ -72,11 +72,11 @@ public class KakaoApi {
     //술집 상세 조회
     @Operation(summary = "술집 상세 조회", description = "술집 상세 조회")
     @GetMapping("/alkol/{apiId}")
-    public List<KakaoResponseDto> getAlkol(@PathVariable String apiId, @AuthenticationPrincipal UserDetailsImpl userDetails){
-        if(userDetails==null){
-            return kakaoApiService.getAlkol(apiId,null);
+    public List<KakaoResponseDto> getAlkol(@PathVariable String apiId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        if (userDetails == null) {
+            return kakaoApiService.getAlkol(apiId, null);
         } else
-            return kakaoApiService.getAlkol(apiId,userDetails.getUser());
+            return kakaoApiService.getAlkol(apiId, userDetails.getUser());
     }
     // 게시글 많은 순으로 술집 조회
     @Operation(summary = "게시글 많은 순 술집 조회", description = "게시글 많은 순으로 술집 조회")
@@ -91,11 +91,23 @@ public class KakaoApi {
     // 모든 술집 조회
     @Operation(summary = "전체 술집 조회", description = "모든 술집 조회")
     @GetMapping("/alkol/all")
-    public List<AlkolResponseDto> getAllAlkol(@RequestParam(value = "page", defaultValue = "1") int page, @RequestParam(value = "size", defaultValue = "10") int size, @AuthenticationPrincipal UserDetailsImpl userDetails){
+    public List<AlkolResponseDto> getAllAlkol(@RequestParam(required = false, defaultValue = "") String placeName,
+                                              @RequestParam(required = false, defaultValue = "") String categoryName,
+                                              @RequestParam(required = false, defaultValue = "") String addressName,
+                                              @RequestParam(required = false, defaultValue = "") String roadAddressName,
+                                              @RequestParam(value = "page", defaultValue = "1") int page, @RequestParam(value = "size", defaultValue = "10") int size, @AuthenticationPrincipal UserDetailsImpl userDetails){
         if(userDetails==null){
-            return kakaoApiService.getAllAlkol(null,page - 1,size);
+            if(placeName==null){
+                return kakaoApiService.getAllAlkol(null,null,null,null,null,page - 1,size);
+            }else{
+                return kakaoApiService.getAllAlkol(placeName,categoryName, addressName,roadAddressName,null,page - 1,size);
+            }
         } else
-            return kakaoApiService.getAllAlkol(userDetails.getUser(), page - 1,size);
+            if(placeName==null){
+                return kakaoApiService.getAllAlkol(null,null,null,null,userDetails.getUser(),page - 1,size);
+            }else{
+                return kakaoApiService.getAllAlkol(placeName, categoryName,addressName,roadAddressName,userDetails.getUser(), page - 1,size);
+            }
     }
 
     //좋아요 순으로 술집 조회
